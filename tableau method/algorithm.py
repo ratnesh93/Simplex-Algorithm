5d64047e33ba2fa0728c0624faebcaa8aade85bd
Ratnesh Chandak <ratnesh.chandak@gmail.com>
2016-02-27 12:03:50 +0530

    final commit
    revised test cases and algorithm

--- tableau method/algorithm.py
+++ tableau method/algorithm.py
@@ -1,5 +1,9 @@
-import os
 '''
+Name : Ratnesh Chandak
+Roll No: CS12B1030
+Btech 4th year
+file : program of simplex method in tableau form
+Note : To run the program change the inputIndex in line 19 for different input file
 
 input format in standard form:
 	x1 x2 .. xn s1 s2 .. sm RHS
@@ -10,6 +14,10 @@ s2
 sm
 
 '''
+import os
+#give the input index of file name, i.e input file name is input+inputIndex where inputIndex is an integer
+inputIndex=1
+
 def divide(A,k):
 	for j in range(len(A)):
 		A[j]/=float(k)
@@ -22,7 +30,7 @@ def mult(A,k):
 	for j in range(len(A)):
 		A[j]*=float(k)
 
-def tableau(M):
+def colIndex(M):
 	rows = len(M)
 	columns = len(M[0])
 	minR=M[0][0]
@@ -31,19 +39,24 @@ def tableau(M):
 		if(M[0][i] < minR):
 			minR=M[0][i]
 			colIndex=i
+	return colIndex
 
-	#print colIndex
-
+def rowIndex(M,colIndex):
+	rows = len(M)
+	columns = len(M[0])
 	minratio = 99999
-	rowIndex = 0
+	rowIndex = -1
 	for i in range(1,rows):
 		if(M[i][colIndex] != 0 ):
 			ratio = M[i][columns-1] / float(M[i][colIndex])
 			if (ratio > 0 and ratio < minratio):
 				minratio =ratio
 				rowIndex=i
+	return rowIndex
 
-	#print rowIndex
+def tableau(M,rowIndex,colIndex):
+	rows = len(M)
+	columns = len(M[0])
 
 	divide(M[rowIndex],M[rowIndex][colIndex])
 
@@ -56,12 +69,10 @@ def tableau(M):
 			mult(temp,M[i][colIndex])
 			sub(M[i],temp)
 
-	#print M
-
 
 M=[]
 #getting input
-f = open(os.getcwd()+'/input5.txt', 'r')
+f = open(os.getcwd()+'/input'+str(inputIndex)+'.txt', 'r')
 for line in f:
         C=[]
         line=line.split(",")
@@ -70,12 +81,35 @@ for line in f:
         M.append(C)
 f.close()
 
-for i in range(len(M[0])):
+i=0
+#check for checking unboundness 
+check =0
+count =0;
+while(1):
 	if(M[0][i] < 0):
-		tableau(M)
+		colIn = colIndex(M)
+		rowIn = rowIndex(M,colIn)
+		if(rowIn == -1):
+			check = 1
+			break
+		else:
+			tableau(M,rowIn,colIn)
+		i=0
+	elif (i==len(M[0])-2):
+		break
+	else:
+		i=i+1
 
+#printing resultant tableau
+
+print "\n",
 print M
-print "Max value is: "
-print M[0][len(M[0])-1]
+if(check ==0):
+	print "\n",
+	print "Max value is: ", 
+	print M[0][len(M[0])-1],
+	print "\n"
+else:
+	print "The problem is unbounded"
 
 
